639e290ee40903f0089dc733224edae83a7c4ad7
JuanColado <juanicolado@gmail.com>
2021-09-16 22:02:36 -0300

    mejora

--- Clase1/app.js
+++ Clase1/app.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 app.use(express.static("public"));
-app.listen(3040,console.log("Esta corriendo en el puerto 3040"));
-app.get("/",function(req,res){
-    res.sendFile((path.join(__dirname,'/views','/index.html')))})
+app.listen(3040, console.log("Esta corriendo en el puerto 3040"));
+ app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views", "/index.html"))});
+
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views", "/login.html"));
+});
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views", "/register.html"));
+});
+app.post("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views", "/register.html"));
+});
+app.post("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});

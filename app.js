215ab28a6bacb2471571f2fb515b85f50e195298
JuanColado <juanicolado@gmail.com>
2021-09-16 22:37:25 -0300

    heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 5000;
 app.use(express.static("public"));
 app.listen(3040, console.log("Esta corriendo en el puerto 3040"));
  app.get("/", function (req, res) {

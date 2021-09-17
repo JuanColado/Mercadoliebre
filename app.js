ceebaf5727b638b9f65603554f10b42596e3759d
JuanColado <juanicolado@gmail.com>
2021-09-16 22:44:53 -0300

    heroku2

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 app.use(express.static("public"));
-app.listen(3040, console.log("Esta corriendo en el puerto 3040"));
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Funciona en el http://localhost:3000/%27')})
  app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/views", "/index.html"))});
 
